cba60a28799059d10fce36587956aae364ab0b07
Sukachev <nikolai_sukachev@mail.ru>
2023-01-30 10:20:29 +0300

    Первая версия со сложением

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace calcEfanov
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите первое число");
+
+            double a = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Введите Второе число");
+
+            double b = Convert.ToDouble(Console.ReadLine());
+
+            double c;
+
+            c = a + b;
+
+            Console.WriteLine("Результат сложения: " + c);
+
+            Console.ReadKey();
+        }
+    }
+}
